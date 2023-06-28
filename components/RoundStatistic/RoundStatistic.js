1aa495ddb760e3cf702489432b407c4ba649bf61
Igor Klyopov <shoot8202@gmail.com>
2023-06-28 17:51:33 +0300

    bugfix/change display of current round statistic

--- components/RoundStatistic/RoundStatistic.js
+++ components/RoundStatistic/RoundStatistic.js
@@ -1,46 +1,40 @@
 import styles from './RoundStatistic.module.scss';
 
 const RoundStatistic = ({ data, roundCount }) => {
-  data=[];
+  const { roundNumber, coinTossNumber, playerWinsCount } = data;
+
+  const casinoWinsCount = coinTossNumber - playerWinsCount;
+  const winRate = playerWinsCount - casinoWinsCount;
+  const getWinnerMessage = () => {
+    if (winRate > 0) {
+      return 'You won this round';
+    } else if (winRate < 0) {
+      return 'Casino won this round';
+    } else {
+      return 'There is no winner in this round';
+    }
+  };
+
+  const winnerMessage = getWinnerMessage();
   return (
     <section className={styles.statistic}>
       <h2 className={styles.title}>Round {roundCount} statistic</h2>
 
-      {data.map(({ roundNumber, coinTossNumber, playerWinsCount }, index) => {
-        const casinoWinsCount = coinTossNumber - playerWinsCount;
-        const winRate = playerWinsCount - casinoWinsCount;
-        const getWinnerMessage = () => {
-          if (winRate > 0) {
-            return 'You won this round';
-          } else if (winRate < 0) {
-            return 'Casino won this round';
-          } else {
-            return 'There is no winner in this round';
-          }
-        };
-
-        const winnerMessage = getWinnerMessage();
-
-        if (data.length > 0 && index === data.length - 1)
-          return (
-            <div key={roundNumber}>
-              <p>
-                Coin toss number :{' '}
-                <span className={styles.count}>{coinTossNumber} </span>
-              </p>
-              <p>
-                You won <span className={styles.count}>{playerWinsCount} </span>
-                set(s)
-              </p>
-              <p>
-                Casino won{' '}
-                <span className={styles.count}>{casinoWinsCount} </span>
-                set(s)
-              </p>
-              <p className={styles.result}>{winnerMessage}</p>
-            </div>
-          );
-      })}
+      <div key={roundNumber}>
+        <p>
+          Coin toss number :{' '}
+          <span className={styles.count}>{coinTossNumber} </span>
+        </p>
+        <p>
+          You won <span className={styles.count}>{playerWinsCount} </span>
+          set(s)
+        </p>
+        <p>
+          Casino won <span className={styles.count}>{casinoWinsCount} </span>
+          set(s)
+        </p>
+        <p className={styles.result}>{winnerMessage}</p>
+      </div>
     </section>
   );
 };
